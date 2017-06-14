38e3c939f0b8db48629a4728940143a6f4a483e2
Vanellope <vane11opeschw33tz@gmail.com>
2017-06-14 21:06:30 +0900

    take those screen size and the vram address from asmhead

--- bootpack.c
+++ bootpack.c
@@ -29,11 +29,15 @@ void RubbMain(void)
 {
 	char *vram;
 	int xsize, ysize;
-
+	short *binfo_scrnx, *binfo_scrny;
+	int *binfo_vram;
 	init_palette();
-	vram = (char *) 0xa0000;
-	xsize = 320;
-	ysize = 200;
+	binfo_scrnx = (short *)0xff4;
+	binfo_scrny = (short *)0xff6;
+	binfo_vram = (int *)0xff8;
+	xsize = *binfo_scrnx;
+	ysize = *binfo_scrny;
+	vram = (char *)*binfo_vram;
 
 	boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
 	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
