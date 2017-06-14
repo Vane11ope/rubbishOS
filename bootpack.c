c5b68637848ec121c0f5152e7929ae082ae72b2c
Vanellope <vane11opeschw33tz@gmail.com>
2017-06-14 20:27:39 +0900

    changed color

--- bootpack.c
+++ bootpack.c
@@ -1,10 +1,17 @@
-void io_hlt();
-void write_mem8(int addr, int data);
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void RubbMain(void)
 {
 	int i;
 	char *p;
+	init_palette();
 	p = (char *)0xa0000;
 	for (i = 0; i <= 0xffff; ++i) {
 		*(p + i) = i & 0x0f;
@@ -12,4 +19,44 @@ void RubbMain(void)
 	for (;;) {
 		io_hlt();
 	}
+}
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16*3] = {
+		0x00, 0x00, 0x00,
+		0xff, 0x00, 0x00,
+		0x00, 0xff, 0x00,
+		0xff, 0xff, 0x00,
+		0x00, 0x00, 0xff,
+		0xff, 0x00, 0xff,
+		0x00, 0xff, 0xff,
+		0xff, 0xff, 0xff,
+		0xc6, 0xc6, 0xc6,
+		0x84, 0x00, 0x00,
+		0x00, 0x84, 0x00,
+		0x84, 0x84, 0x00,
+		0x00, 0x00, 0x84,
+		0x84, 0x00, 0x84,
+		0x00, 0x84, 0x84,
+		0x84, 0x84, 0x84
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for(i = start; i <= end; ++i) {
+		io_out8(0x3c9, rgb[0] / 4);
+		io_out8(0x3c9, rgb[1] / 4);
+		io_out8(0x3c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
 }
--- func.nas
+++ func.nas
@@ -4,10 +4,72 @@
 
 [FILE "func.nas"]
 
-	GLOBAL _io_hlt
+	GLOBAL _io_hlt, _io_cli, _io_sti, _io_stihlt
+	GLOBAL _io_in8, _io_in16, _io_in32
+	GLOBAL _io_out8, _io_out16, _io_out32
+	GLOBAL _io_load_eflags, _io_store_eflags
 
 [SECTION .text]
 
 _io_hlt:
 	HLT
+	RET
+
+_io_cli:
+	CLI
+	RET
+
+_io_sti:
+	STI
+	RET
+
+_io_stihlt:
+	STI
+	HLT
+	RET
+
+_io_in8: ; void io_in8(int port);
+	MOV EDX,[ESP+4]
+	MOV EAX,0
+	IN  AL,DX
+	RET
+
+_io_in16: ; void io_in16(int port);
+	MOV EDX,[ESP+4]
+	MOV EAX,0
+	IN  AX,DX
+	RET
+
+_io_in32: ; void io_in32(int port);
+	MOV EDX,[ESP+4]
+	IN  EAX,DX
+	RET
+
+_io_out8: ; void io_out8(int port, int data);
+	MOV EDX,[ESP+4]
+	MOV AL,[ESP+8]
+	OUT DX,AL
+	RET
+
+_io_out16: ; void io_out16(int port, int data);
+	MOV EDX,[ESP+4]
+	MOV EAX,[ESP+8]
+	OUT DX,AX
+	RET
+
+_io_out32: ; void io_out32(int port, int data);
+	MOV EDX,[ESP+4]
+	MOV EAX,[ESP+8]
+	OUT DX,EAX
+	RET
+
+_io_load_eflags: ; int io_load_eflags(void);
+	PUSHFD       ; PUSH EFLAGS
+	POP EAX
+	RET
+
+_io_store_eflags: ; void io_store_eflags(int eflags);
+	MOV EAX,[ESP+4]
+	PUSH EAX
+	POPFD ; POP EFLAGS
 	RET
