5891359dad37ad1818f48c90ba5312ef7558beb1
Vanellope <vane11opeschw33tz@gmail.com>
2017-06-17 04:59:47 +0900

    mouse added

--- bootpack.c
+++ bootpack.c
@@ -26,9 +26,11 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void init_screen(char *vram, int x, int y);
+void init_mouse(char *mouse, char bc);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
+void putblock8_8 (char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char* buf, int bxsize);
 
 struct BOOTINFO {
 	char cyls, leds, vmode, reserve;
@@ -42,6 +44,10 @@ void RubbMain(void)
 
 	init_palette();
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+
+	char *mouse[256];
+	init_mouse(mouse, COL8_000000);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, 140, 100, mouse, 16);
 
 	//static char font_A[16] = {
 	//	0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
@@ -122,6 +128,43 @@ void init_screen(char *vram, int x, int y)
 	boxfill8(vram, x, COL8_FFFFFF, x - 3, y - 24, x - 3, y - 3);
 }
 
+void init_mouse(char *mouse, char bc)
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+    int x, y;
+	for (y = 0; y < 16; ++y) {
+		for (x = 0; x < 16; ++x) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
 {
 	int x, y;
@@ -158,6 +201,17 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s
 	for (; *s != 0x00; ++s) {
 		putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
 		x += 8;
+	}
+	return;
+}
+
+void putblock8_8 (char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char* buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; ++y) {
+		for (x = 0; x < pxsize; ++x) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
 	}
 	return;
 }
