11c4035f61131e8f030cfa976c1c6ce1853e02cf
Vanellope <vane11opeschw33tz@gmail.com>
2017-07-11 21:25:52 +0900

    console scrolls up as the cursor gets to the bottom of it

--- console.c
+++ console.c
@@ -6,7 +6,7 @@ void console_task(struct SHEET *sheet)
 {
 	struct TIMER *timer;
 	struct TASK *task = task_now();
-	int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
+	int i, x, y, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
 	char s[2];
 
 	fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -47,12 +47,24 @@ void console_task(struct SHEET *sheet)
 						cursor_x -= 8;
 					}
 				} else if (i == 10 + 256) { // Enter
+					putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ");
 					if (cursor_y < 28 + 112) {
-						putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ");
 						cursor_y += 16;
-						putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, ">");
-						cursor_x = 16;
+					} else {
+						for (y = 28; y < 28 + 112; ++y) {
+							for (x = 8; x < 8 + 240; ++x) {
+								sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
+							}
+						}
+						for (y = 28 + 112; y < 28 + 128; ++y) {
+							for (x = 8; x < 8 + 240; ++x) {
+								sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+							}
+						}
+						sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
 					}
+					putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, ">");
+					cursor_x = 16;
 				} else {
 					if (cursor_x < 240) {
 						s[0] = i - 256;
