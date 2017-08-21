b00568f8f781233c32a7e2af48d7ab20f8d6547f
Vanellope <vane11opeschw33tz@gmail.com>
2017-08-21 13:13:29 +0900

    alloca is applied for the window app too

--- open_window/window.c
+++ open_window/window.c
@@ -1,11 +1,9 @@
 #include "../app.h"
 
-char buf[150 * 50];
-
 void RubbMain(void)
 {
 	int window;
-
+	char buf[150 * 50];
 	window = api_open_window(buf, 150, 50, -1, "fuck you");
 	for (;;) {
 		if (api_getkey(1) == 0x0a) { break; }
